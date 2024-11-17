9f6ae163b4bf6cc49239d2dc57e3f86a354fb356
Marvinus11 <viniciusmartins880@yahoo.com.br>
2024-11-17 12:29:45 -0300

    First commit

---
+++ src/test/java/com/marvinus/dslist/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marvinus.dslist.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
